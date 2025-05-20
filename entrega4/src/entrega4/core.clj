ea9a7efaec88130362db0206d9c7d548cbf10360
hmndzzl <men241265@uvg.edu.gt>
2025-05-20 12:06:46 -0600

    Crear programa de ejemplo

--- entrega4/src/entrega4/core.clj
+++ entrega4/src/entrega4/core.clj
@@ -1,7 +1,26 @@
 (ns entrega4.core
+  (:require [clojure.set :as set])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def usuarios
+  {"Ana"    #{"Luis" "Carlos" "Maria"}
+   "Luis"   #{"Ana" "Carlos"}
+   "Carlos" #{"Luis" "Ana" "Pedro"}
+   "Pedro"  #{"Carlos"}
+   "Maria"  #{"Ana"}})
+
+(defn amigos-en-comun [usuario1 usuario2]
+  (clojure.set/intersection (usuarios usuario1) (usuarios usuario2)))
+
+(defn usuario-con-mas-amigos []
+  (apply max-key #(count (val %)) usuarios))
+
+(defn -main []
+  (println "Número de amigos por usuario:")
+  (doseq [[usuario amigos] usuarios]
+    (println usuario ":" (count amigos) "amigos"))
+
+  (let [[u mas-amigos] (usuario-con-mas-amigos)]
+    (println "\nUsuario con más amigos:" u "con" (count mas-amigos) "amigos"))
+
+  (println "\nAmigos en común entre Ana y Carlos:" (amigos-en-comun "Ana" "Carlos")))
